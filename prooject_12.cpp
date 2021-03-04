508f352fc674f41910959c1026a66e8a1536166a
SergeyK123 <75069371+SergeyK123@users.noreply.github.com>
2021-03-04 17:34:51 +0600

    Update prooject_12.cpp

--- prooject_12.cpp
+++ prooject_12.cpp
@@ -2,11 +2,8 @@
 #include <string>
 using namespace std;
 
-
-int main()
-{ 
+int* foo() {
 	int* p_a = new int();
-	
 	cin >> *p_a;
 	if (*p_a > 0) {
 		cout << *p_a << endl;
@@ -15,6 +12,16 @@ int main()
 		*p_a = -(*p_a);
 		cout << *p_a << endl;
 	}
+	return p_a;
+}
+
+int main()
+{ 
+	int* p_a = 0;
+	
+	
+	p_a = foo();
+
 	delete p_a;
 
 	p_a = 0;
